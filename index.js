9bd2ae93ac3995b42aba2b679cf8ad4e8b6cac0a
Marcos Martinez <mmtz2108@gmail.com>
2022-04-06 23:08:12 -0500

    commit tree 017e073c45e635a0b19d27064c2d6f01a8728d93

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// const names = ["Marcos", "Laura", "Seve"];
+// const event = ["birthday"];
+
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+
+        const newPhrase = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(newPhrase)
+    }
+    return newArray
+};
+function countDown(positiveInteger) {
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger--);
+
+    }
+} 
